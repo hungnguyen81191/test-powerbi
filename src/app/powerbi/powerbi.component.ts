175bad35a054a1d794f870db097ea338cadd2d4a
HungNV <HungNV164@fpt.com>
2024-08-13 06:20:22 +0700

    test

---
+++ src/app/powerbi/powerbi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-powerbi',
+  standalone: true,
+  imports: [],
+  templateUrl: './powerbi.component.html',
+  styleUrl: './powerbi.component.css'
+})
+export class PowerbiComponent implements OnInit{
+  
+  ngOnInit(): void {
+    
+  }
+}
---
+++ src/app/powerbi/powerbi.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PowerBIEmbedModule } from 'powerbi-client-angular';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PowerBIEmbedModule
+  ]
+})
+export class PowerbiModule { }
